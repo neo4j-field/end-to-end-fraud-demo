111bdf9e4acf7256d602ebe3698a792f2002f9ef
Dave Voutila <voutilad@gmail.com>
2022-04-06 13:59:23 -0400

    bump clojure version now that they patched 1.11.0

--- dump2csv/project.clj
+++ dump2csv/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/data.csv "1.0.0"]
                  [org.neo4j.driver/neo4j-java-driver "4.4.5"]]
   :main dump2csv.core
